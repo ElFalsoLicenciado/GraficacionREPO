2ec49afc79df588aa53089d83b4db6ef9d4aed20
Albertico <chopytrollface@gmail.com>
2025-10-08 23:21:03 -0600

    Evaluacion practica 2

--- haarcascade.py
+++ haarcascade.py
@@ -12,60 +12,45 @@ delta_y = 0
 i_x = 1
 i_y = 1
 
+size_ton = 0
+delta_ton = 1
+inc_ton = 5
+
 while True:
     ret, img = cap.read()
     gris = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     rostros = rostro.detectMultiScale(gris, 1.3, 5)
     time += 1
     
-    
-    
-    
-    
     for(x,y,w,h) in rostros:
+        
+        size_ton = int(h*0.4)
         
         if(dir_x == delta_x): i_x = 0
         if(dir_y == delta_y): i_y = 0
 
-        print("prepucio")
-        
+    
         if((dir_x == delta_x) and (dir_y == delta_y)):
-            print("Entramos a chambear")
-            dir_x = r.randint(int(-1*(w//16)),int(1*(w//16)))
-            dir_y = r.randint(int(-1*(w//16)),int(1*(w//16)))
-            
-            if(dir_x == delta_x): i_x = 0
-            if(dir_y == delta_y): i_y = 0
+            dir_x = r.randint(int(-1*(w//36)),int(1*(w//36)))
+            dir_y = r.randint(int(-1*(w//36)),int(1*(w//36)))
             
             if(dir_x > delta_x): 
                 i_x = 1
-                print("IX")
-            else: 
-                i_x = -1
-                print("-IX")
+            else:
+                if(dir_x < delta_x): 
+                    i_x = -1
+                else: i_x = 0
                 
             if(dir_y > delta_y): 
                 i_y = 1
-                print("IY")
             else: 
-                i_y = -1
-                print("-IY")
+                if(dir_y < delta_y):
+                    i_y = -1
+                else: i_y = 0
 
-        
-    
-            
         delta_x += i_x
         delta_y += i_y
         
-        print(f"DIR: {dir_x},{dir_y}")
-        print("")
-        print(f"DELTA: {delta_x},{delta_y}")
-        print("")
-        print(f"I: {i_x},{i_y}")
-        print("")
-        
-        
-        res = int((w+h)/8)
         
         img = cv.rectangle(img, (x,y), (x+w, y+h), (234, 23,23), 5)
         img = cv.rectangle(img, (x,int(y+h/2)), (x+w, y+h), (0,255,0),5 )
@@ -73,20 +58,32 @@ while True:
         img = cv.circle(img, (x + int(w*0.3), y + int(h*0.4)) , w//16, (0, 0, 0), -1 )
         img = cv.circle(img, (x + int(w*0.7), y + int(h*0.4)) , w//16, (0, 0, 0), -1 )
         
-        if( time <= 10):
+        if( time <= 20):
             img = cv.circle(img, (x + int(w*0.3), y + int(h*0.4)) , w//17, (255, 255, 255), -1 ) # Esclerotica
             img = cv.circle(img, (x + int(w*0.3) + delta_x, y + int(h*0.4) + delta_y) , w//50, (0, 0, 255), -1 )     # Pupila
             
             img = cv.circle(img, (x + int(w*0.7), y + int(h*0.4)) , w//17, (255, 255, 255), -1 ) # Esclerotica
             img = cv.circle(img, (x + int(w*0.7) + delta_x, y + int(h*0.4) + delta_y) , w//50, (0, 0, 255), -1 )     # Pupila
         else:
-            if(time >= 15): time = 0
+            if(time >= 25): time = 0
             
         
         img = cv.rectangle(img, (x + int(w*0.36), y + int(h*0.375)), (x + int(w*0.6), y + int(h*0.65)), (234,0,234), 5) # Nariz
         
         # img = cv.circle(img, (x + int(w*0.6), y + int(h*0.65)) , w//50, (0, 0, 255), -1 )
         img = cv.rectangle(img, (x+ int(w*.35), y+int(h*0.78)), (x + int(w*0.6), y + int(h*0.85)), (87,122,185), 5) # Boca
+        img = cv.circle(img, (x + int(w*0.475), y + int(h*0.815)), int(h*0.10), (0,0,0), -1 )
+        
+        delta_ton += inc_ton
+        if(delta_ton > size_ton):
+            inc_ton = -5
+        else:
+            if(delta_ton < 0):
+                    inc_ton = 5
+                
+        
+        
+        img = cv.rectangle(img, (x+ int(w*.40), y+int(h*0.80)), (x + int(w*0.55), y + int(h*0.85) + delta_ton), (0,0,255), -1)
         
         # img = cv.rectangle(img, (x+ int(w*.09), y+int(h*0.375)), (x + int(w*0.15), y + int(h*0.65)), (71,190,155), 5) # Oreja 1
         # img = cv.rectangle(img, (x+ int(w*.85), y+int(h*0.375)), (x + int(w*0.91), y + int(h*0.65)), (71,190,155), 5) # Oreja 2
